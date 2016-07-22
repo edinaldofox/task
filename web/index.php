eb58ca8c34141d643f2536c2c8a3b11c618c98c5
edinaldo <edinaldofox@gmail.com>
2016-07-21 23:35:49 -0300

    configuracoes quando nao ouver registro.

--- web/index.php
+++ web/index.php
@@ -37,4 +37,9 @@ $app->mount('/usuario', new UsuarioController());
 $app->mount('/task', new TaskController());
 
 
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 $app->run();
